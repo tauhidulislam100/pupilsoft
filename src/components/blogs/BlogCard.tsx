986c083dc8606a78a72725705745673a54240db6
tauhidulislam100 <tauhidulislam100@gmail.com>
2023-11-20 10:06:26 +0600

    added wordpress api

--- src/components/blogs/BlogCard.tsx
+++ src/components/blogs/BlogCard.tsx
@@ -1,37 +1,35 @@
 import Link from 'next/link';
 import React from 'react';
+import dayjs from 'dayjs';
 
-const BlogCard = ({
-  id,
-  img,
-  time,
-  name,
-  title,
-  description,
-}: {
-  id: number;
-  img: string;
-  time: string;
-  name: string;
-  title: string;
-  description: string;
-}) => {
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+const BlogCard = ({ data }: any) => {
+  const { featuredImage, title, excerpt, author, date, slug } = data;
   return (
     <div className="font-avenir">
-      <Link href={`/blogs/${id}`}>
+      <Link href={`/blogs/${slug}`}>
         <div className="h-[200px] md:h-[399px] w-full rounded-lg overflow-hidden">
-          <img src={img} className="w-full h-full object-cover" alt="" />
+          <img
+            src={featuredImage?.node.sourceUrl}
+            className="w-full h-full object-cover"
+            alt=""
+          />
         </div>
         <div className="mt-2 md:mt-6 space-y-5">
           <div className="text-sm md:text-lg font-avenir text-[#445169] flex items-center gap-2">
-            {time} read{' '}
+            {dayjs(date).fromNow()}
             <div className="w-1.5 h-1.5 rounded-full bg-[#445169]" />
-            by {name}
+            by {author.node.name}
           </div>
           <h2 className="text-[#1C1B4E] text-2xl md:text-[40px] md:leading-[60px] font-bold">
             {title}
           </h2>
-          <p className="text-gray-300">{description}</p>
+          <div
+            className="text-dark"
+            dangerouslySetInnerHTML={{ __html: excerpt }}
+          />
         </div>
       </Link>
     </div>
